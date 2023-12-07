a137f74b6a1af727db4c5a910aba1fe0226d1597
Chinh <chinh99562@donga.edu.vn>
2023-12-07 23:20:33 +0700

    deploy-n

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/book-store': '/'
+// export default defineConfig({
+//   plugins: [react()],
+//   base: process.env.NODE_ENV === 'production' ? '/book-store': '/'
+// })
+
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [react()],
+    base: "/"
+  }
+
+  if (command !== 'serve') {
+    config.base = '/book-store/'
+  }
+
+  return config
 })
